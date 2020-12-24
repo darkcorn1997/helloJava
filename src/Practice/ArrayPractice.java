4c5df6a8037cd72a37ff47da4c2139b018b56f63
chenjie.shi <1334033788@qq.com>
2020-12-24 10:22:11 +0800

    123

--- src/Practice/ArrayPractice.java
+++ src/Practice/ArrayPractice.java
@@ -1,5 +1,7 @@
 package Practice;
 
+import java.util.Arrays;
+
 public class ArrayPractice {
     public static char printGrade(double grade) {
         return grade >= 90 ? 'A'
@@ -7,25 +9,34 @@ public class ArrayPractice {
                 : grade >= 40 && grade < 60 ? 'C' : 'D';
     }
 
-    public static void main(String[] args) {
+    public static void MaxAndMinAndReverse() {
         int[] array = new int[5];
-        array[0] = (int) (Math.random() * 100);
-        array[1] = (int) (Math.random() * 100);
-        array[2] = (int) (Math.random() * 100);
-        array[3] = (int) (Math.random() * 100);
-        array[4] = (int) (Math.random() * 100);
-
-        System.out.println("数组中的各个随机数是:");
-        for (int i : array) System.out.println(i);
-
-        int max = array[0];
-        int min = array[0];
-        for (int i : array) {
-            if (max < i) { max=i; }
-            if (min > i) { min=i; }
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
         }
 
-        System.out.println("本练习的目的是，通过for循环，遍历数组，找出最小值: " + min + " 最大值：" + max);
-        System.out.println(printGrade(40));
+        System.out.println("数组中的各个随机数是:" + Arrays.toString(array));
+
+        int max = array[0], min = array[0];
+        for (int i : array) {
+            if (max < i)
+                max=i;
+            if (min > i)
+                min=i;
+        }
+        System.out.println("通过for循环，遍历数组，找出最小值: " + min + " 最大值：" + max);
+
+
+        int[] newArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[array.length-1-i];
+        }
+        System.out.println("反转数组：" + Arrays.toString(newArray));
+    }
+
+
+    public static void main(String[] args) {
+        //System.out.println(ArrayPractice.printGrade(40));
+        ArrayPractice.MaxAndMinAndReverse();
     }
 }
