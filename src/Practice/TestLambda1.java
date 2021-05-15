d853cdf5bc86232a776baaaa0eb7e41c51c6985d
chenjie.shi <1334033788@qq.com>
2021-05-15 14:42:43 +0800

    fishing

--- src/Practice/TestLambda1.java
+++ src/Practice/TestLambda1.java
@@ -15,7 +15,7 @@ public class TestLambda1 {
     }
 
     public static void main(String[] args) {
-        Random r = new Random();
+        var r = new Random();
         List<Hero> heroes = new ArrayList<>();
         for(int i=0; i<10; i++) {
             heroes.add(new Hero("hero"+i, r.nextInt(1000), r.nextInt(100)));
@@ -25,16 +25,23 @@ public class TestLambda1 {
         filter(heroes);
         System.out.println("聚合方式 管道筛选");
         /*
-        对元素进行筛选：
-filter 匹配
-distinct 去除重复(根据equals判断)
-sorted 自然排序
-sorted(Comparator<T>) 指定排序
-limit 保留
-skip 忽略
+对元素进行筛选：
+ filter 匹配
+ distinct 去除重复(根据equals判断)
+ sorted 自然排序
+ sorted(Comparator<T>) 指定排序
+ limit 保留
+ skip 忽略
 转换为其他形式的流
-mapToDouble 转换为double的流
-map 转换为任意类型的流
+ mapToDouble 转换为double的流
+ map 转换为任意类型的流
+结束操作
+ forEach() 遍历每个元素
+ toArray() 转换为数组
+ min(Comparator<T>) 取最小的元素
+ max(Comparator<T>) 取最大的元素
+ count() 总数
+ findFirst() 第一个元素
          */
         System.out.println(        heroes
                 .stream()
--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,9 +1,6 @@
 package collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import charactor.Hero;
 
@@ -19,22 +16,60 @@ public class TestCollection {
         Hero specialHero = new Hero("special hero");
         heroes.add(specialHero);
 
-        //遍历
+        //迭代器遍历
+        Iterator<Hero> iterator = heroes.iterator();
+        while (iterator.hasNext()) {
+            Hero h = iterator.next();
+            System.out.println(h);
+        }
+
+        //增强for遍历
         for (Hero hero : heroes) {
             System.out.println(hero);
         }
 
         //转化为数组
-        Hero[] hs = (Hero[]) heroes.toArray(new Hero[]{});
+        Hero[] hs = heroes.toArray(new Hero[]{});
         System.out.println("数组:" + Arrays.toString(hs));
     }
 
     public static void testLinkedList() {
-        List<Hero> heroes = new LinkedList<>();
+        Deque<Hero> heroes = new LinkedList<>(); //LinkedList实现了双向链表结构Deque以及List,可以很方便的在头尾插入删除数据
+        // 链表结构: 与数组结构相比较，数组结构，就好像是电影院，每个位置都有标示，每个位置之间的间隔都是一样的
+        // 而链表就相当于佛珠，每个珠子，只连接前一个和后一个，不用关心除此之外的其他佛珠在哪里
+        heroes.add(new Hero("0"));
+        heroes.addFirst(new Hero("1"));
+        heroes.addLast(new Hero("2"));
+        heroes.add(new Hero("3"));
+        System.out.println(heroes);
+        System.out.println(heroes.getFirst());
+        System.out.println(heroes.getLast());
+        heroes.removeFirst();
+        heroes.removeLast();
+        System.out.println(heroes);
+    }
+
+    public static void testLinkedList2() {
+        Queue<Hero> heroes = new LinkedList<>();//Deque继承Queue,因此LinkedList还实现了Queue 先进先出队列 FIFO
+        //加在队列最后
+        heroes.offer(new Hero("0"));
+        heroes.offer(new Hero("1"));
+        heroes.add(new Hero("2"));
+        System.out.println(heroes);
+        //看看第一个 但不会取出来
+        Hero hero = heroes.peek();
+        System.out.println(hero);
+        System.out.println(heroes);
+        //取出第一个
+        Hero hero1 = heroes.poll();
+        System.out.println(hero);
+        System.out.println(heroes);
     }
 
 
     public static void main(String[] args) {
-        TestCollection.testArrayList();
+        //TestCollection.testArrayList();
+        //TestCollection.testLinkedList();
+        TestCollection.testLinkedList2();
     }
 }
